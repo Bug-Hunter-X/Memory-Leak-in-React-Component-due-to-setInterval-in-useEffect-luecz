bcedfe6fc654afaea110cbe8b187a6030d310add
BugHunter-X <askzenai@gmail.com>
2025-01-24 06:00:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect usage of setInterval
+    setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000); // Updates every second
+  }, []);
+
+  return <div>Count: {count}</div>;
+}
+```
